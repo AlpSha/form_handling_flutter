52d2cdff535376e2932019444aea78673c840910
Alp Sahin <alp@sahin.club>
2025-02-27 16:58:02 +0300

    Fixed DateTime validator issue on non required fields

--- lib/src/application/date_validator.dart
+++ lib/src/application/date_validator.dart
@@ -2,7 +2,7 @@ import 'package:form_handling/src/domain/input_failure.dart';
 
 import 'custom_validator.dart';
 
-class DateValidator extends CustomValidator<DateTime, DateTimeInputFailure> {
+class DateValidator extends CustomValidator<DateTime?, DateTimeInputFailure> {
   DateValidator({
     required super.isRequired,
     this.minDate,
@@ -13,12 +13,14 @@ class DateValidator extends CustomValidator<DateTime, DateTimeInputFailure> {
   final DateTime? maxDate;
 
   @override
-  ValidationResult<DateTime, DateTimeInputFailure> validateAndGetResult(
-      DateTime? value) {
+  ValidationResult<DateTime?, DateTimeInputFailure> validateAndGetResult(DateTime? value) {
     if (value == null) {
-      return const ValidationResult.failure(
-        DateTimeInputFailure.empty(),
-      );
+      if (isRequired) {
+        return const ValidationResult.failure(
+          DateTimeInputFailure.empty(),
+        );
+      }
+      return ValidationResult.success(null);
     }
     final minDate = this.minDate;
     if (minDate != null && value.isBefore(minDate)) {
